0af497530aebaf208d494ebe125fad354e796141
AnidaMalkic <malkic.anida@gmail.com>
2023-09-28 14:34:20 -0500

    second commit

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,54 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    public void testSettingJobId() {
+        Job firstJob = new Job();
+        Job secondJob = new Job();
+
+        assertNotEquals(firstJob.getId(), secondJob.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job  theJob = new Job("Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertTrue(theJob instanceof Job);
+        assertTrue(theJob.getName() instanceof String);
+        assertTrue(theJob.getEmployer() instanceof Employer);
+        assertTrue(theJob.getLocation() instanceof Location);
+        assertTrue(theJob.getPositionType() instanceof PositionType);
+        assertTrue(theJob.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals("Product tester", theJob.getName());
+        assertEquals("ACME", theJob.getEmployer().getValue());
+        assertEquals("Desert", theJob.getLocation().getValue());
+        assertEquals("Quality control", theJob.getPositionType().getValue());
+        assertEquals("Persistence", theJob.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job firstJob = new Job("Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        Job secondJob = new Job("Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertFalse(firstJob.equals(secondJob));
+    }
 }
